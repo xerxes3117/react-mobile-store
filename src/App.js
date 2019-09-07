9c0f739d745a7dab9d21a8f4ec27419f368b7dd9
xerxes3117 <vsnsit@gmail.com>
2019-09-08 01:56:43 +0530

    timestamp 49.30

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/Navbar';
+import Productlist from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-2">Col 1</div>
-        <div className="col-2">
-          <span>
-            <i className="fas fa-home" />
-          </span>
-          </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Productlist} />
+        <Route path="/details" component={Details} />
+        <Route path="/store" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//To use our routes in whole application we need to wrap our 
+//App component inside Router component(i.e. BrowserRouter)
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
