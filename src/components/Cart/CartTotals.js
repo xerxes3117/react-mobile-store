8c500305d76e22e302cf4f195a9a180aee647dda
xerxes3117 <vsnsit@gmail.com>
2019-09-14 00:58:03 +0530

    paypal button added

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PaypalButton from './PaypalButton';
 
-const CartTotals = ({ value }) => {
+const CartTotals = ({ value, history }) => {
     const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
     return (
         <React.Fragment>
@@ -30,6 +31,9 @@ const CartTotals = ({ value }) => {
                                 total : <strong>$ {cartTotal}</strong>
                             </span>
                         </h5>
+                        <PaypalButton total={cartTotal}
+                            clearCart={clearCart}
+                            history={history} />
                     </div>
                 </div>
             </div>
