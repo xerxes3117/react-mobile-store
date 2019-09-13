e1296aa0a71968928ce4472c467079e045ae4275
xerxes3117 <vsnsit@gmail.com>
2019-09-13 18:53:34 +0530

    doing netlify setup

--- src/context.js
+++ src/context.js
@@ -23,7 +23,7 @@ class ProductProvider extends Component {
         let tempProducts = [];
         storeProducts.forEach(item => {
             const singleItem = { ...item };
-            tempProducts = [...tempProducts, singleItem]; //same as products.push(singleItem) ?
+            tempProducts = [...tempProducts, singleItem];
         });
         this.setState(() => {
             return { products: tempProducts }
@@ -40,7 +40,6 @@ class ProductProvider extends Component {
     }
     addToCart = id => {
         let tempProducts = [...this.state.products];
-        //const index = tempProducts.indexOf(this.getItem(id));
         const product = tempProducts.find(item => item.id === id);
         product.inCart = true;
         product.count = 1;
@@ -48,30 +47,102 @@ class ProductProvider extends Component {
         product.total = price;
         this.setState(() => {
             return { products: tempProducts, cart: [...this.state.cart, product] };
-        }, () => { console.log(this.state) });
+        }, () => {
+            this.addTotals();
+        });
     }
-    openModal = id =>{
+    openModal = id => {
         const product = this.getItem(id);
         this.setState(() => {
-            return {modalProduct: product, modalOpen: true};
+            return { modalProduct: product, modalOpen: true };
         })
     }
     closeModal = () => {
-        this.setState(()  => {
-            return {modalOpen: false};
+        this.setState(() => {
+            return { modalOpen: false };
         })
     }
     increment = (id) => {
-        
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count += 1;
+        product.total = product.count * product.price;
+
+        this.setState(() => {
+            return {
+                cart: [...tempCart]
+            }
+        }, () => {
+            this.addTotals();
+        })
     }
     decrement = (id) => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
 
+        product.count -= 1;
+        if (product.count === 0) {
+            this.removeItem(id);
+        } else {
+            product.total = product.count * product.price;
+
+            this.setState(() => {
+                return {
+                    cart: [...tempCart]
+                }
+            }, () => {
+                this.addTotals();
+            })
+        }
     }
-    removeItem = (id)  => {
+    removeItem = (id) => {
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
 
+        tempCart = tempCart.filter(item => item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(() => {
+            return {
+                cart: [...tempCart],
+                products: [...tempProducts]
+            }
+        }, () => {
+            this.addTotals();
+        })
     }
-    clearCart   = () => {
-
+    clearCart = () => {
+        this.setState(() => {
+            return {
+                cart: []
+            }
+        }, () => {
+            this.setProducts();
+            this.addTotals();
+        })
+    }
+    addTotals = () => {
+        let subTotal = 0;
+        this.state.cart.map(item => (subTotal += item.total));
+        const tempTax = parseFloat((subTotal * 0.1).toFixed(2));
+        const total = subTotal + tempTax;
+        this.setState(() => {
+            return {
+                cartSubTotal: subTotal,
+                cartTax: tempTax,
+                cartTotal: total
+            }
+        })
     }
     render() {
         return (
